76971b32119aa1ee4205d2289bba6c9a1b5f0be4
juan <paco>
2023-02-20 17:59:01 -0300

    cambios diseño - diseño js y diseñoMiniaturas

--- src/components/body/diseños/diseños.js
+++ src/components/body/diseños/diseños.js
@@ -27,17 +27,63 @@ export default function Diseños(props){
 
             <h2>Diseños</h2>
 
-            <div className="content-diseños">
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
-                <DiseñosMiniaturas img={null}/>
+            <div className = "content-diseños">
+               
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+            
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
+
+            <DiseñosMiniaturas 
+                large  = {"https://i.pinimg.com/564x/1c/c0/07/1cc007fe5efccb28aa27777799912e60.jpg"} 
+                short1 = {"https://i.pinimg.com/564x/0b/be/75/0bbe75e176b3b6c9884dbfd5bc59b519.jpg"} 
+                short2 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"} 
+                short3 = {"https://i.pinimg.com/564x/1e/eb/cf/1eebcfbbc7b116cc4077ac9e7db533c5.jpg"}
+            />
             </div>
            
         </div>
--- src/components/body/diseñosMiniaturas/diseñosMiniaturas.js
+++ src/components/body/diseñosMiniaturas/diseñosMiniaturas.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './diseñosMiniaturas.css';
-export default function DiseñosMiniaturas(){
+
+const ImgC=(props)=>{
+    return <img src={props.url} alt="imagen-diseño"/>;
+}
+
+export default function DiseñosMiniaturas(props){
+
+  
 
     return(
-        <div className="diseño-item-content"></div>
+        <div className="diseño-item-content">
+            
+            <div className="large-image">
+                <ImgC url = {props.large}/>
+            </div>
+
+            <div className = "short-images">
+                <ImgC url = {props.short1}/>
+                <ImgC url = {props.short2}/>
+                <ImgC url = {props.short3}/>
+            </div>
+        </div>
     )
 }
