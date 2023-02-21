5de84eb06b104465d229508776d2088d95e303af
juan <paco>
2023-02-21 18:22:52 -0300

    Diseño frontend terminado

--- src/components/body/proyectos/proyectos.js
+++ src/components/body/proyectos/proyectos.js
@@ -5,8 +5,15 @@ import ProyectosMiniaturas from '../proyectosMiniaturas/proyectoMiniaturas';
 export default function Proyectos(props){
 
     const [animationClass,setAnimationClass] = useState("proyectos-montar");
-    const [img,setImg] = useState("");
-
+    const [data,setData] = useState({
+        titulo:null,
+        img:null,
+        tag:null,
+        descripcion:null,
+        data_publicacion:null
+    });
+   
+    
     const goodbye = (n) => {   
         setAnimationClass("proyectos-desmontar");
         setTimeout(()=>{
@@ -14,9 +21,13 @@ export default function Proyectos(props){
         },3000);  
     }
 
+    const ejemploText= "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo, haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock, un profesor de Latin de la Universidad de Hampden-Sydney en Virginia, encontró una de las palabras más oscuras de la lengua del latín, consecteur, en un pasaje de Lorem Ipsum, y al seguir leyendo distintos textos del latín, descubrió la fuente indudable. Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de de Finnibus Bonorum et Malorum (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo. Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento. La primera linea del Lorem Ipsum, Lorem ipsum dolor sit amet.., viene de una linea en la sección 1.10.32";
     /* Peticion servidor */
+    // Datos para peticion
+    
    return( 
         <div className={` proyectos-page ${animationClass}`}>
+
             <menu>
                 <li className="volver" onClick = { (e) => goodbye(0) }> Inicio</li>
                 <li>&gt;</li>
@@ -26,10 +37,10 @@ export default function Proyectos(props){
             <h2>proyectos</h2>
 
             <div className="content-proyectos">
-                <ProyectosMiniaturas img={null} titulo={null} descripcion={null}>1</ProyectosMiniaturas>
-                <ProyectosMiniaturas img={null} titulo={null} descripcion={null}>2</ProyectosMiniaturas>
-                <ProyectosMiniaturas img={null} titulo={null} descripcion={null}>3</ProyectosMiniaturas>
-                <ProyectosMiniaturas img={null} titulo={null} descripcion={null}>4</ProyectosMiniaturas>
+                <ProyectosMiniaturas img = {"https://github.com/CodeBAou/Archito/raw/main/img-readme/menuEj1.png"} titulo = {"Archito, un script de mantenimiento para arch"} fecha={"21-2-2023"} tag={"bash Scriting"} descripcion={ejemploText} repo={"https://github.com/CodeBAou/Archito"}>1</ProyectosMiniaturas>
+                <ProyectosMiniaturas img = {"https://github.com/CodeBAou/Archito/raw/main/img-readme/menuEj1.png"} titulo = {"Archito, un script de mantenimiento para arch"} fecha={"21-2-2023"} tag={"bash Scriting"} descripcion={ejemploText} repo={"https://github.com/CodeBAou/Archito"}>1</ProyectosMiniaturas>
+                <ProyectosMiniaturas img = {"https://github.com/CodeBAou/Archito/raw/main/img-readme/menuEj1.png"} titulo = {"Archito, un script de mantenimiento para arch"} fecha={"21-2-2023"} tag={"bash Scriting"} descripcion={ejemploText} repo={"https://github.com/CodeBAou/Archito"}>1</ProyectosMiniaturas>
+                <ProyectosMiniaturas img = {data.img} titulo = {data.titulo} data_publicacion = {data.data_publicacion} tag = {data.tag} descripcion = {data.descripcion}>4</ProyectosMiniaturas>
             </div>
            
         </div>
--- src/components/body/proyectosMiniaturas/proyectoMiniaturas.js
+++ src/components/body/proyectosMiniaturas/proyectoMiniaturas.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './proyectosMiniaturas.css';
-
+import github_icon from './github.svg';
+import visibilityOn from './visibility_on.svg';
+import visibilityOff from './visibility_off.svg';
 export default function ProyectosMiniaturas(props){
 
     const [data, setData] = useState({
@@ -19,8 +21,24 @@ export default function ProyectosMiniaturas(props){
     }
 
     return(
-        <div className="proyecto-item-content">
+        <div className="proyecto-item-content" onClick={(e) => verPage((e))}>
+            <div>
+                <img src={props.img} alt="Imagen principal del proyecto"/>
+                <h3>{props.titulo}</h3>
+                <h4>{props.fecha}</h4>
+                <h5>{props.tag}</h5>
+            </div>
+
+            <p>{props.descripcion}</p>
             
+            <div className="icongithub">
+                <a href={props.repo} target="_blank" onClick={(e)=>{ e.stopPropagation()}}> 
+                    <img src={ github_icon } alt="icono de github"/>
+                </a>
+                <a href="#" target=""  >
+                    <img src={visibilityOn} alt="icono Entrar al post" onClick={(e)=>{e.stopPropagation()}} />
+                </a>
+            </div>
         </div>
     )
 } 
